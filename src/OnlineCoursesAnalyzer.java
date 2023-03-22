9b04559e5cce8c8c5bcff5a68045e348c462ef5f
TheBang7 <2802446015@qq.com>
2023-03-22 20:31:30 +0800

    1-3 finish

--- src/OnlineCoursesAnalyzer.java
+++ src/OnlineCoursesAnalyzer.java
@@ -45,17 +45,58 @@ public class OnlineCoursesAnalyzer {
 
     //1
     public Map<String, Integer> getPtcpCountByInst() {
-        return null;
+
+        Map<String, Integer> m = new LinkedHashMap<String, Integer>();
+        Map<String, Integer> m2 = new LinkedHashMap<String, Integer>();
+        for (Course c : courses) {
+            if (!m.containsKey(c.institution))
+                m.put(c.institution, c.participants);
+            else m.put(c.institution, m.get(c.institution) + c.participants);
+        }
+        m.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByKey()).forEachOrdered(e -> m2.put(e.getKey(), e.getValue()));
+        return m2;
     }
 
     //2
     public Map<String, Integer> getPtcpCountByInstAndSubject() {
-        return null;
+        Map<String, Integer> m = new LinkedHashMap<String, Integer>();
+        Map<String, Integer> m2 = new LinkedHashMap<String, Integer>();
+        for (Course c : courses) {
+            String s = c.institution + "-" + c.subject;
+            if (!m.containsKey(s))
+                m.put(s, c.participants);
+            else m.put(s, m.get(s) + c.participants);
+        }
+        m.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).forEachOrdered(e -> m2.put(e.getKey(), e.getValue()));
+        return m2;
     }
 
     //3
     public Map<String, List<List<String>>> getCourseListOfInstructor() {
-        return null;
+        Map<String, List<List<String>>> m = new HashMap<>();
+        for (Course c : courses) {
+            String[] strings = c.instructors.split(",");
+            int t = 1;
+            if (strings.length == 0) continue;
+            if (strings.length == 1) t = 0;
+            for (String s : strings) {
+                s=s.trim();
+                if (!m.containsKey(s)) {
+                    ArrayList<List<String>> a = new ArrayList<List<String>>();
+                    m.put(s, a);
+                    a.add(new ArrayList<String>());//l1
+                    a.add(new ArrayList<String>());//l2
+                    a.get(t).add(c.title);
+                } else {
+                    if (!m.get(s).get(t).contains(c.title))
+                        m.get(s).get(t).add(c.title);
+                }
+            }
+        }
+        m.forEach((key, value) -> Collections.sort(value.get(0)));
+        m.forEach((key, value) -> Collections.sort(value.get(1)));
+        return m;
+
     }
 
     //4
@@ -137,6 +178,191 @@ class Course {
         this.medianAge = medianAge;
         this.percentMale = percentMale;
         this.percentFemale = percentFemale;
+        this.percentDegree = percentDegree;
+
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Date getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(Date launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInstructors() {
+        return instructors;
+    }
+
+    public void setInstructors(String instructors) {
+        this.instructors = instructors;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getHonorCode() {
+        return honorCode;
+    }
+
+    public void setHonorCode(int honorCode) {
+        this.honorCode = honorCode;
+    }
+
+    public int getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(int participants) {
+        this.participants = participants;
+    }
+
+    public int getAudited() {
+        return audited;
+    }
+
+    public void setAudited(int audited) {
+        this.audited = audited;
+    }
+
+    public int getCertified() {
+        return certified;
+    }
+
+    public void setCertified(int certified) {
+        this.certified = certified;
+    }
+
+    public double getPercentAudited() {
+        return percentAudited;
+    }
+
+    public void setPercentAudited(double percentAudited) {
+        this.percentAudited = percentAudited;
+    }
+
+    public double getPercentCertified() {
+        return percentCertified;
+    }
+
+    public void setPercentCertified(double percentCertified) {
+        this.percentCertified = percentCertified;
+    }
+
+    public double getPercentCertified50() {
+        return percentCertified50;
+    }
+
+    public void setPercentCertified50(double percentCertified50) {
+        this.percentCertified50 = percentCertified50;
+    }
+
+    public double getPercentVideo() {
+        return percentVideo;
+    }
+
+    public void setPercentVideo(double percentVideo) {
+        this.percentVideo = percentVideo;
+    }
+
+    public double getPercentForum() {
+        return percentForum;
+    }
+
+    public void setPercentForum(double percentForum) {
+        this.percentForum = percentForum;
+    }
+
+    public double getGradeHigherZero() {
+        return gradeHigherZero;
+    }
+
+    public void setGradeHigherZero(double gradeHigherZero) {
+        this.gradeHigherZero = gradeHigherZero;
+    }
+
+    public double getTotalHours() {
+        return totalHours;
+    }
+
+    public void setTotalHours(double totalHours) {
+        this.totalHours = totalHours;
+    }
+
+    public double getMedianHoursCertification() {
+        return medianHoursCertification;
+    }
+
+    public void setMedianHoursCertification(double medianHoursCertification) {
+        this.medianHoursCertification = medianHoursCertification;
+    }
+
+    public double getMedianAge() {
+        return medianAge;
+    }
+
+    public void setMedianAge(double medianAge) {
+        this.medianAge = medianAge;
+    }
+
+    public double getPercentMale() {
+        return percentMale;
+    }
+
+    public void setPercentMale(double percentMale) {
+        this.percentMale = percentMale;
+    }
+
+    public double getPercentFemale() {
+        return percentFemale;
+    }
+
+    public void setPercentFemale(double percentFemale) {
+        this.percentFemale = percentFemale;
+    }
+
+    public double getPercentDegree() {
+        return percentDegree;
+    }
+
+    public void setPercentDegree(double percentDegree) {
         this.percentDegree = percentDegree;
     }
 }
